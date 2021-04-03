6e8e5f83ef6d90f7ccec23b46b33e5dd7e9380c9
gauravpokharel@ambition.edu.np <gauravpokharel@ambition.edu.np>
2021-04-03 12:20:09 +0545

    Test Class Added

---
+++ StudentManagement/StudentManagement/Controllers/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Controllers
+{
+    public class TestClass
+    {
+        public int MyProperty { get; set; }
+        public void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
